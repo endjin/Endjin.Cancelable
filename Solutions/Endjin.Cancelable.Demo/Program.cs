c420f373a5ddc8050a2d3f419282feb2c41326fa
Howard.vanRooijen <Howard.vanRooijen@endjin.com>
2015-04-13 09:55:13 +0100

    Add warning message to ensure user is running the Azure Emulator

--- Solutions/Endjin.Cancelable.Demo/Program.cs
+++ Solutions/Endjin.Cancelable.Demo/Program.cs
@@ -16,13 +16,16 @@
     {
         public static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ensure you are running the Azure Storage Emulator!");
+            Console.ResetColor();
+
             ApplicationServiceLocator.InitializeAsync(new Container(), new DesktopBootstrapper()).Wait();
 
             var cancelable = ApplicationServiceLocator.Container.Resolve<ICancelable>();
             var cancellationToken = "E75FF4F5-755E-4FB9-ABE0-24BD81F4D045";
 
             cancelable.CreateToken(cancellationToken);
-
             cancelable.RunUntilCompleteOrCancelledAsync(DoSomethingLongRunningAsync, cancellationToken).Wait();
 
             Console.WriteLine("Press Any Key to Exit!");
@@ -32,6 +35,7 @@
         private static async Task DoSomethingLongRunningAsync(CancellationToken cancellationToken)
         {
             int counter = 0;
+
             while (!cancellationToken.IsCancellationRequested)
             {
                 Console.WriteLine("Doing something {0}", DateTime.Now.ToString("T"));
